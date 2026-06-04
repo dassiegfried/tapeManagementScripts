d1d93330cae079d3f502b311f4ca619f95e3d790
mike <mike@AM4.am4.lan>
2026-06-04 17:38:27 +0200

    fix: apprise push config loading ; fix formatting according to shellcheck

--- leftWrite.sh
+++ leftWrite.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo $1
-set -a && source .env && set +a
-shaRemote=$(cat $1.sha1.remote | cut -d " " -f 1)
-shaFromFile=$(cat $1.sha1 | cut -d " " -f 1)
+echo "$1"
+shaRemote=$(cat "$1".sha1.remote | cut -d " " -f 1)
+shaFromFile=$(cat "$1".sha1 | cut -d " " -f 1)
 if ./tapeChecks.sh ; then
+  apprise -b "tape left check success Start tape right" -t tapeLeft --config ./apprise.env
   if [ "$shaRemote" = "$shaFromFile" ]; then
     echo "Local and remote check success"
 
     sleep 5
-    mbuffer -P 86 -m 7G -o /dev/nst0 -s 524288 -i $1 &&  mt -f /dev/nst0 weof 1 && sudo mt -f /dev/nst0 asf 0 && echo "tape left spooled to pos 0" && mbuffer -P 86 -m 7G -i /dev/nst0 -s 524288 | sha1sum > $1.sha1.L && mt -f /dev/nst0 eject
-    shaFromTape=$(cat $1.sha1.L | cut -d " " -f 1)
-    shaFromFile=$(cat $1.sha1 | cut -d " " -f 1)
+    mbuffer -P 86 -m 7G -o /dev/nst0 -s 524288 -i "$1" &&  mt -f /dev/nst0 weof 1 && sudo mt -f /dev/nst0 asf 0 && echo "tape left spooled to pos 0" && mbuffer -P 86 -m 7G -i /dev/nst0 -s 524288 | sha1sum > "$1".sha1.L && mt -f /dev/nst0 eject
+    shaFromTape=$(cat "$1".sha1.L | cut -d " " -f 1)
+    shaFromFile=$(cat "$1".sha1 | cut -d " " -f 1)
 
     if [ "$shaFromTape" = "$shaFromFile" ]; then
         echo "Checksums Match!"
@@ -18,14 +18,14 @@ if ./tapeChecks.sh ; then
     else
         echo "Checksum check FAILED!"
         echo "sha from tape: "
-        echo $shaFromTape
+        echo "$shaFromTape"
         echo "sha from File:"
-        echo $shaFromFile
-        apprise -b "tape left checksum missmatch" -t tapeLeftFail $notify
+        echo "$shaFromFile"
+        apprise -b "tape left checksum missmatch" -t tapeLeftFail --config ./apprise.env
     fi
   else
     echo "FAIL!!! Local File and Remote Checksums do not match!"
   fi
 else
- apprise -b "tape check left failed" -t tapeLeftFail $notify
+ apprise -b "tape check left failed" -t tapeLeftFail --config ./apprise.env
 fi
--- rightWrite.sh
+++ rightWrite.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-echo $1
-set -a && source .env && set +a
-shaRemote=$(cat $1.sha1.remote | cut -d " " -f 1)
-shaFromFile=$(cat $1.sha1 | cut -d " " -f 1)
+echo "$1"
+shaRemote=$(cat "$1".sha1.remote | cut -d " " -f 1)
+shaFromFile=$(cat "$1".sha1 | cut -d " " -f 1)
 if ./tapeChecksRight.sh ; then
   if [ "$shaRemote" = "$shaFromFile" ]; then
     echo "local and remote check success"
     echo "sleeping 200sek to ensure other tape write finishes before this one because we delete after and they dont"
     sleep 400
-    mbuffer -P 86 -m 7G -o /dev/nst1 -s 524288 -i $1 &&  mt -f /dev/nst1 weof 1 && sudo mt -f /dev/nst1 asf 0 && echo "tape Right spooled to pos 0" && mbuffer -P 86 -m 7G -i /dev/nst1 -s 524288 | sha1sum > $1.sha1.R && mt -f /dev/nst1 eject
+    mbuffer -P 86 -m 7G -o /dev/nst1 -s 524288 -i "$1" &&  mt -f /dev/nst1 weof 1 && sudo mt -f /dev/nst1 asf 0 && echo "tape Right spooled to pos 0" && mbuffer -P 86 -m 7G -i /dev/nst1 -s 524288 | sha1sum > "$1".sha1.R && mt -f /dev/nst1 eject
     # && rm $1
-    shaFromTape=$(cat $1.sha1.L | cut -d " " -f 1)
-    shaFromTapeRight=$(cat $1.sha1.R | cut -d " " -f 1)
+    shaFromTape=$(cat "$1".sha1.L | cut -d " " -f 1)
+    shaFromTapeRight=$(cat "$1".sha1.R | cut -d " " -f 1)
 
     if [ "$shaFromTape" = "$shaFromFile" ]; then
         echo "Checksums Left Match to File"
@@ -22,28 +21,28 @@ if ./tapeChecksRight.sh ; then
           echo "Checksums Right match to Remote"
           echo "all checksums match"
           echo "delete File"
-          rm $1
-          apprise -b "All tapes written & checksums match" -t tapeFinsih $notify
+          rm "$1"
+          apprise -b "All tapes written & checksums match" -t tapeFinsih --config ./apprise.env
          else
           echo "checksum check FAILED! Right Tape Checksum dont match remote"
-          apprise -b "checksum check FAILED! Right Tape Checksum dont match remote" -t tapeRightFail $notify
+          apprise -b "checksum check FAILED! Right Tape Checksum dont match remote" -t tapeRightFail --config ./apprise.env
          fi
         else
          echo "checksum check FAILED! Right Tape Checksum dont match File"
-         apprise -b "checksum check FAILED! Right Tape Checksum dont match File" -t tapeRightFail $notify
+         apprise -b "checksum check FAILED! Right Tape Checksum dont match File" -t tapeRightFail --config ./apprise.env
         fi
     else
         echo "Checksum check FAILED!"
         echo "sha from tape: "
-        echo $shaFromTape
+        echo "$shaFromTape"
         echo "sha from File:"
-        echo $shaFromFile
-        apprise -b "tape right checksum missmatch" -t tapeRightFail $notify
+        echo "$shaFromFile"
+        apprise -b "tape right checksum missmatch" -t tapeRightFail --config ./apprise.env
     fi
   else
     echo "FAIL!! Remote and local file Checksums do not match!"
-    apprise -b "local and remote checksums dont match" -t tapeFail $notify
+    apprise -b "local and remote checksums dont match" -t tapeFail --config ./apprise.env
   fi
 else
- apprise -b "tape check right failed" -t tapeRightFail $notify
+ apprise -b "tape check right failed" -t tapeRightFail --config ./apprise.env
 fi
