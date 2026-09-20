a4b8256cc6e531c83870aa0206de480bca430c5d
LB@thonkPad <lbAtThonkPad@bieringer.dev>
2026-09-20 20:28:52 +0200

    fix: apprise config path because of CD change

--- tapeChecks.sh
+++ tapeChecks.sh
@@ -2,7 +2,7 @@
 cd /home/truenas_admin/tapeManagementScripts
 sudo sg_read_attr /dev/nst0 && /bin/bash ./autoloaderReadWriteTest.sh && sudo /mnt/app2/home/tapeManagementScripts/ITDT/itdt -f /dev/sg0 devicestatistics &&  sudo mt -f /dev/nst0 load && echo "load success" && sudo mt -f /dev/nst0 asf 0 && echo "tape spooled to pos 0" && sudo sg_read_attr /dev/nst0
 if [ $? -ne 0 ]; then
-    apprise -b "tape check failed" -t tapeChecks --config ../apprise.conf
+    apprise -b "tape check failed" -t tapeChecks --config ./apprise.conf
     echo "something is wrong with this tape." > /dev/tty
     echo "please manually replace this tape with a working tape" > /dev/tty
     echo "you have TWO more tape tries before we will resume writing to the inserted tape no matter the test results" > /dev/tty
@@ -10,7 +10,7 @@ if [ $? -ne 0 ]; then
     read < /dev/tty
     sudo sg_read_attr /dev/nst0 && /bin/bash ./autoloaderReadWriteTest.sh && sudo /mnt/app2/home/tapeManagementScripts/ITDT/itdt -f /dev/sg0 devicestatistics &&  sudo mt -f /dev/nst0 load && echo "load success" && sudo mt -f /dev/nst0 asf 0 && echo "tape spooled to pos 0" && sudo sg_read_attr /dev/nst0
     if [ $? -ne 0 ]; then
-        apprise -b "tape check failed" -t tapeChecks --config ../apprise.conf
+        apprise -b "tape check failed" -t tapeChecks --config ./apprise.conf
         echo "something is wrong with this tape." > /dev/tty
         echo "please manually replace this tape with a working tape" > /dev/tty
         echo "you have ONE more tape tries before we will resume writing to the inserted tape no matter the test results" > /dev/tty
@@ -18,7 +18,7 @@ if [ $? -ne 0 ]; then
         read < /dev/tty
         sudo sg_read_attr /dev/nst0 && /bin/bash ./autoloaderReadWriteTest.sh && sudo /mnt/app2/home/tapeManagementScripts/ITDT/itdt -f /dev/sg0 devicestatistics &&  sudo mt -f /dev/nst0 load && echo "load success" && sudo mt -f /dev/nst0 asf 0 && echo "tape spooled to pos 0" && sudo sg_read_attr /dev/nst0
         if [ $? -ne 0 ]; then
-            apprise -b "tape check failed" -t tapeChecks --config ../apprise.conf
+            apprise -b "tape check failed" -t tapeChecks --config ./apprise.conf
             echo "something is wrong with this tape." > /dev/tty
             echo "please manually replace this tape with a working tape" > /dev/tty
             echo "you have ONE more tape tries before we will resume writing to the inserted tape no matter the test results" > /dev/tty
