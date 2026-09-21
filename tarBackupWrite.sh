971abe928945510b9b998cc2ae5a49344f0df2f9
LB@thonkPad <lbAtThonkPad@bieringer.dev>
2026-09-21 12:42:17 +0200

    feat: decrease zstd compression level

--- tarBackupWrite.sh
+++ tarBackupWrite.sh
@@ -9,5 +9,5 @@ echo "check loaded tape"
 if [ $? -ne 0 ]; then
     echo "check failed load undamaged tape to start write"
 else 
-    tar -h --hard-dereferenc -I "zstd -9 -T12" -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
+    tar -h --hard-dereferenc -I "zstd -7 -T12" -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
 fi
