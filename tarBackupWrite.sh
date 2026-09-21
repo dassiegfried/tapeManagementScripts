f3fdae38cdae647f1ffd8f1dcd579bf12c8d29ab
LB@thonkPad <lbAtThonkPad@bieringer.dev>
2026-09-21 09:37:46 +0200

    fix: path

--- tarBackupWrite.sh
+++ tarBackupWrite.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -ne 0 ]
 fi
 cd /mnt/replica
 echo "check loaded tape"
-/bin/bash ./autoloaderReadWriteTest.sh
+/bin/bash /mnt/app2/home/tapeManagementScripts/autoloaderReadWriteTest.sh
 if [ $? -ne 0 ]; then
     tar -h --hard-dereferenc -I "zstd -9 -T12" -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
 else 
