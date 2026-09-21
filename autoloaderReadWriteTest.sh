1b8d009a0bd580a2e729a8b69fb528aa71ab3cae
LB@thonkPad <lbAtThonkPad@bieringer.dev>
2026-09-21 09:44:35 +0200

    fix: logic error when starting new backup on tape check start by spooling to pos 0

--- autoloaderReadWriteTest.sh
+++ autoloaderReadWriteTest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /mnt/app2/home/tapeManagementScripts/
 md5Source=$(cat 10G.md5 | cut -d " " -f 1)
+sudo mt -f /dev/nst0 asf 0 && echo "tape spooled to pos 0" 
 cat 10G.md5 && cat 10G | sudo mbuffer -o /dev/nst0 -H && sudo mt -f /dev/nst0 weof 1 && sudo mt -f /dev/nst0 asf 0 && echo "tape spooled to pos 0" && sudo mbuffer -i /dev/nst0 -H | md5sum > tapeReadWriteTest.md5
 md5FromTape=$(cat tapeReadWriteTest.md5 | cut -d " " -f 1)
 if [ "$md5Source" = "$md5FromTape" ]; then
--- tarBackupWrite.sh
+++ tarBackupWrite.sh
@@ -7,7 +7,7 @@ cd /mnt/replica
 echo "check loaded tape"
 /bin/bash /mnt/app2/home/tapeManagementScripts/autoloaderReadWriteTest.sh
 if [ $? -ne 0 ]; then
-    tar -h --hard-dereferenc -I "zstd -9 -T12" -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
-else 
     echo "check failed load undamaged tape to start write"
+else 
+    tar -h --hard-dereferenc -I "zstd -9 -T12" -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
 fi
