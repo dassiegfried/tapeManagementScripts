fc276c6f160baab9c2736cd321246d887bb253d0
LB@thonkPad <lbAtThonkPad@bieringer.dev>
2026-09-21 13:18:07 +0200

    feat: removing compression because it decreases input to much and therefore starves tape more often

--- tarBackupWrite.sh
+++ tarBackupWrite.sh
@@ -10,5 +10,5 @@ if [ $? -ne 0 ]; then
     echo "check failed load undamaged tape to start write"
 else 
     apprise -b "tape checks successfull enter password to start backup" -t tapeEnterPassword --config ./apprise.conf
-    tar -h --hard-dereferenc -I "zstd -7 -T12" -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
+    tar -h --hard-dereference -c -C new . | age -p | mbuffer -P 95 -m 800G -T /mnt/app2/home/autoloader -s 524288 -o /dev/nst0 -A "/mnt/app2/home/tapeManagementScripts/autoLoaderNext.sh"
 fi
